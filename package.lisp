edd41ba016557200d5e8baf3b8daeb39d3140270
Dmytro Pinskyy <demetrius@neverblued.info>
2012-03-13 01:06:15 +0200

    Шапка с лицензией.

--- package.lisp
+++ package.lisp
@@ -1,3 +1,7 @@
+;; (c) Дмитрий Пинский <demetrius@neverblued.info>
+;; Допускаю использование и распространение согласно
+;; LLGPL -> http://opensource.franz.com/preamble.html
+
 (defpackage #:blah
   (:use #:cl #:blackjack #:iter)
   (:export #:language #:with-language
